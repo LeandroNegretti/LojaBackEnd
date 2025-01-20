3a41617b3d7b626572a49a0d34d33eb435ee4eb6
LeandroNegretti <negrettileandro3@gmail.com>
2025-01-20 20:22:33 -0300

    update: usuariorepository criado

--- src/repositories/UsuarioRepository.js
+++ src/repositories/UsuarioRepository.js
@@ -7,17 +7,61 @@ class UsuarioRepository {
       return await Usuario.create(data);
     } catch (error) {
       console.error("Erro ao criar usuário!", error);
-      throw new error("Erro ao criar usuário!");
+      throw new Error("Erro ao criar usuário!");
     }
   }
 
   // Método para buscar todos usuários
-  async buscarUsuarios() {
+  async buscarTodosUsuarios() {
     try {
       return await Usuario.findAll();
     } catch (error) {
         console.error('Erro ao buscar usuarios', error);
-        throw new error('erro ao buscar usuario');
+        throw new Error('erro ao buscar usuario');
+    }
+  }
+
+  // Método para buscar usuário pelo ID
+  async buscarUsuarioPorId(id) {
+    try {
+      const usuario = await Usuario.findByPk();
+      if (!usuario) {
+        return null; // Retorna null caso o usuário não seja encontrado
+      }
+      return usuario;
+    } catch (error) {
+      console.error(`Erro ao buscar usuário com id ${id}:`, error);
+      throw new Error('Erro ao buscar usuários');
+    }
+  }
+
+  // Método para atualizar um usuário
+  async atualizarUsuario(id, data) {
+    try {
+      const usuario = await Usuario.findByPk(id);
+      if (usuario) {
+        await usuario.update(data, { where: {id} });
+        return await Usuario.findByPk(id) // Retorna o usário atualizado
+      }
+      return null;
+    } catch (error) {
+      console.error(`Erro ao atualizar usuário com id ${id}:`, error);
+      throw new Error('Erro ao atualizar usuário');
+    }
+  }
+
+  // Método para excluir um usuário
+  async excluirUsuario(id) {
+    try {
+      const usuario = await Usuario.findByPk(id);
+      if (usuario) {
+        await usuario.destroy(); // Exclui o usuário
+        return usuario;
+      }
+      return null; // Retorna null caso o usuário não seja encontrado
+    } catch (error) {
+      console.error(`Erro ao excluir um usuário com id ${id}:`, error);
+      throw new Error('Erro ao excluir usuário');
     }
   }
 }
