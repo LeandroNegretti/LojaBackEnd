09d17c2b2d236887deeb9de9d44282496879801c
LeandroNegretti <negrettileandro3@gmail.com>
2025-01-20 20:50:41 -0300

    update: ajuste realizado

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = requeri("express");
-const sequelize = require("./src/config/database"); // Conexão com o banco de dados
-const dotenv = require("dotenv");
+const express = require('express');
+const sequelize = require('./src/config/database'); // Conexão com o banco de dados
+const dotenv = require('dotenv');
 
 // Carregar variáveis de ambiente
 dotenv.config();
@@ -9,34 +9,33 @@ const app = express();
 //configuração de Middleware
 app.use(express.json());
 
-const usuarioRoutes = require("./src/routes/UsuarioRoutes");
+const usuarioRoutes = require('./src/routes/UsuarioRoutes');
 
-app.use("api/", usuarioRoutes);
+app.use('api/', usuarioRoutes);
 
 // Função para conectar ao banco de dados
 const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Conexão com o banco de dados bem-sucedida!");
-  } catch (error) {
-    console.error("Não foi possível conectar ao banco de dados:", error);
-    process.exit(1); // Encerra o servidor caso falhe a conexão com o banco
-  }
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados bem-sucedida!');
+    } catch (error) {
+        console.error('Não foi possível conectar ao banco de dados:', error);
+        process.exit(1); // Encerra o servidor caso falhe a conexão com o banco
+    }
 };
 
 connectDB();
 
 // Alterar as tabelas sem apagar os dados
-sequelize
-  .sync({ alter: true }) // Alteração segura (não apaga dados)
-  .then(() => {
-    console.log("Banco de dados sincronizado!");
-  })
-  .catch((error) => {
-    console.error("Erro ao sincronizar o banco de dados:", error);
-  });
+sequelize.sync({ alter : true })  // Alteração segura (não apaga dados)
+    .then(() => {
+        console.log('Banco de dados sincronizado!');
+    })
+    .catch((error) => {
+        console.error('Erro ao sincronizar o banco de dados:', error);
+    });
 
-const PORT = 3000; // Definindo a porta diretamente no código
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+    const PORT = 3000;  // Definindo a porta diretamente no código
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
