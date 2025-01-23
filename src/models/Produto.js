7f3ed3c8e2b7257b240c0f48053be8a068075a10
LeandroNegretti <negrettileandro3@gmail.com>
2025-01-23 19:47:56 -0300

    update: modelo de produto criado

---
+++ src/models/Produto.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Produto = sequelize.define(
+  "Produto",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    descricao: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    preco: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    quantidadeEstoque: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sku: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    categoria: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imagem: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },
+  {
+    tableName: "produtos",
+    timestamps: true, // adiciona createdAt e updatedAt
+  }
+);
+module.exports = Produto;
