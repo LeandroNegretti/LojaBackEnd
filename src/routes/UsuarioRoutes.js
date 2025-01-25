0b0bcad34ffce61c4e230f0475fe8dc4f14ab167
LeandroNegretti <negrettileandro3@gmail.com>
2025-01-25 16:36:59 -0300

    update: authMiddleware add na rota de usuario

--- src/routes/UsuarioRoutes.js
+++ src/routes/UsuarioRoutes.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const usuarioController = require('../controllers/UsuarioController');
- 
+const express = require("express");
+const usuarioController = require("../controllers/UsuarioController");
+const authMiddleware = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 // Rota para listar todos os usuários
-router.get('/usuarios', usuarioController.buscarTodosUsuarios);
+router.get("/usuarios", authMiddleware, usuarioController.buscarTodosUsuarios);
 
 // Rota para criar um usuario
-router.post('/usuarios', usuarioController.criarUsuario);
+router.post("/usuarios", usuarioController.criarUsuario);
 
 // Rota para buscar um usuário por ID
-router.get('/usuarios/:id', usuarioController.buscarUsuarioPorId);
+router.get(
+  "/usuarios/:id",
+  authMiddleware,
+  usuarioController.buscarUsuarioPorId
+);
 
 // Rota para atualizar um usuário por ID
-router.put('/usuarios/:id', usuarioController.atualizarUsuario);
+router.put("/usuarios/:id", authMiddleware, usuarioController.atualizarUsuario);
 
 // Rota para excluir um usuário por ID
-router.delete('/usuarios/:id', usuarioController.excluirUsuario);
+router.delete(
+  "/usuarios/:id",
+  authMiddleware,
+  usuarioController.excluirUsuario
+);
 
 // Rota para login de usuário
-router.post('/usuarios/login', usuarioController.loginUsuario);
-
+router.post("/usuarios/login", usuarioController.loginUsuario);
 
 module.exports = router;
